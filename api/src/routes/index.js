77c523126282c922462645ec523d4a4efe71eb2a
Khatanna <carlos65357@gmail.com>
2022-05-20 10:39:54 -0400

    hello

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,6 +3,9 @@ const pokemonsRouter = require("./pokemons.js");
 const typesRouter = require("./types.js");
 const router = Router();
 
+router.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 router.use("/pokemons", pokemonsRouter);
 router.use("/types", typesRouter);
 module.exports = router;
